673eb01389d4d50f0e46903b1f7bc5a938e79056
Djmarcelocruz <113671994+Djmarcelocruz@users.noreply.github.com>
2026-07-03 06:43:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -77,3 +77,116 @@ localStorage.setItem("usuario",JSON.stringify(usuario));
 abrirPainel();
 
 }
+function abrirPainel(){
+
+if(usuario.role=="supervisor"){
+
+document.getElementById("app").innerHTML=`
+
+<div class="container">
+
+<div class="card">
+
+<h2>Bem-vindo</h2>
+
+<h3>${usuario.full_name}</h3>
+
+<div class="menu">
+
+<button onclick="telaCadastroMaquina()">
+🚜 Cadastrar Máquina
+</button>
+
+<button onclick="listarMaquinas()">
+📋 Máquinas
+</button>
+
+<button onclick="alert('Em breve')">
+👥 Usuários
+</button>
+
+<button onclick="alert('Em breve')">
+✅ Checklists
+</button>
+
+<button onclick="alert('Em breve')">
+🔧 Manutenções
+</button>
+
+<button onclick="alert('Em breve')">
+📊 Relatórios
+</button>
+
+<button onclick="logout()">
+🚪 Sair
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+}
+
+}
+
+function logout(){
+
+localStorage.removeItem("usuario");
+
+location.reload();
+
+}
+
+function telaCadastroMaquina(){
+
+document.getElementById("app").innerHTML=`
+
+<div class="container">
+
+<div class="card">
+
+<h2>Cadastro de Máquina</h2>
+
+<input id="nome" placeholder="Nome da máquina">
+
+<input id="codigo" placeholder="Código">
+
+<select id="tipo">
+
+<option value="Empilhadeira">Empilhadeira</option>
+
+<option value="Carrinho Elétrico">Carrinho Elétrico</option>
+
+</select>
+
+<input id="fabricante" placeholder="Fabricante">
+
+<input id="modelo" placeholder="Modelo">
+
+<input id="horimetro" type="number" placeholder="Horímetro Inicial">
+
+<input id="preventiva" type="number" value="250" placeholder="Intervalo Preventivo">
+
+<button onclick="salvarMaquina()">
+
+Salvar Máquina
+
+</button>
+
+<button onclick="abrirPainel()">
+
+Voltar
+
+</button>
+
+</div>
+
+</div>
+
+`;
+
+}
